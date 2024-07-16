7d230d6633d58e753458b92a8c3fb3a321184e55
miiiiiYT <41822967+miiiiiYT@users.noreply.github.com>
2024-07-16 22:04:31 +0200

    add lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+//! rustpay is a more secure and sound way to handle centralized transactions.
+//! 
+//! 
+
+pub mod bank;
+pub mod merchant;
+pub mod transaction;
+pub mod user;
+mod error;
+mod iban;
+mod traits;
+
+pub use error::Error;
+pub use iban::IBAN;

6a99c0c11fdd2f2a618924825c0155ba5d25aacb
miiiiiYT <41822967+miiiiiYT@users.noreply.github.com>
2024-07-16 22:04:07 +0200

    add error

---
+++ src/error.rs
@@ -0,0 +1,22 @@
+use std::fmt::Display;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Error {
+    NotAnIBAN,
+    WrongIBANSize,
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.as_str())
+    }
+}
+
+impl Error {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Self::NotAnIBAN => "the provided string was unable to be converted into an iban",
+            Self::WrongIBANSize => "the provided string is too long or too short to be an iban",
+        }
+    }
+}

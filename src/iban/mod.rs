ca60f9cdbfbbc7608d1e12b6e87cd3f51ebfd43d
miiiiiYT <41822967+miiiiiYT@users.noreply.github.com>
2024-07-17 22:19:09 +0200

    move from impls to own module

--- src/iban/mod.rs
+++ src/iban/mod.rs
@@ -1,11 +1,11 @@
 mod countrycodes;
+mod from_implementations;
 
 use std::ops::{Deref, DerefMut};
 
 use countrycodes::CountryCode;
 use serde::{Deserialize, Serialize};
 
-use crate::Error;
 pub use crate::traits::ToBytes;
 
 /// IBAN length by country code
@@ -151,78 +151,6 @@ impl DerefMut for IBAN {
     }    
 }
 
-impl TryFrom<String> for IBAN {
-    type Error = crate::Error;
-    
-    fn try_from(value: String) -> Result<Self, Self::Error> {
-        let input = value.replace(" ", "").to_uppercase();
-
-        if input.len() > 34 {
-            return Err(Error::WrongIBANSize);
-        }
-
-        let mut iban = IBAN::new();
-        for (i, ch) in input.chars().enumerate() {
-            if !ch.is_ascii_alphanumeric() {
-                return Err(Error::NotAnIBAN);
-            }
-            iban[i] = ch;
-        }
-
-        Ok(iban)
-    }
-}
-
-impl TryFrom<&str> for IBAN {
-    type Error = crate::Error;
-    
-    fn try_from(value: &str) -> Result<Self, Self::Error> {
-        let input = value.replace(" ", "").to_uppercase();
-
-        if input.len() > 34 {
-            return Err(Error::WrongIBANSize);
-        }
-
-        let mut iban = IBAN::new();
-        for (i, ch) in input.chars().enumerate() {
-            if !ch.is_ascii_alphanumeric() {
-                return Err(Error::NotAnIBAN);
-            }
-            iban[i] = ch;
-        }
-
-        Ok(iban)
-    }
-}
-
-impl TryFrom<Vec<u8>> for IBAN {
-    type Error = crate::Error;
-
-    fn try_from(value: Vec<u8>) -> Result<Self, Self::Error> {
-        if value.len() > 34 {
-            return Err(Error::WrongIBANSize);
-        }
-
-        let mut iban = IBAN::new();
-        let input = value.iter().map(|b| *b as char).collect::<Vec<char>>();
-        for i in 0..input.len() {
-            // cant be none since the range is known
-            let ch = input.get(i).unwrap();
-            if !ch.is_ascii() {
-                return Err(Error::NotAnIBAN);
-            }
-            iban[i] = *ch;
-        }
-
-        Ok(iban)
-    }
-}
-
-impl From<[char; 34]> for IBAN {
-    fn from(value: [char; 34]) -> Self {
-        IBAN(value)
-    }
-}
 
 impl Serialize for IBAN {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
@@ -267,6 +195,7 @@ impl<'de> Deserialize<'de> for IBAN {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::Error;
 
     #[test]
     fn zero_length() {
